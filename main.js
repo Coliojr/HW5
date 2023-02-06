72c4cd7e47d95820affd029e87aaa8b7cb973ec0
Kelley-rosa <kelley.rosa@bellevuecollege.edu>
2023-02-06 01:14:17 -0600

    added css and the rest of the changed

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let newTrip = function() {
     let trip = new Trip(name, destination, month, transportation, numPeople);
 
     if (!trip.isValid()) {
-        alert("Please enter informatin in all input fields. " 
+        alert("Please enter information in all input fields. " 
         + "Number of guests must be greater than 0");
     }
     else {
@@ -29,5 +29,31 @@ let newTrip = function() {
 document.getElementById("buttonShow").addEventListener("click", function(){
     let display = tripArr[0].toString();
     document.getElementById("show").value = display;
+    
+});
+
+document.getElementById("edit").addEventListener("mouseover", function(){
+    let border = "------------------------------------------<br>"
+    let displayAll = "<h2>Catologed Trips</h2>" + border;
+        
+    for (let i = 0; i < tripArr.length; i++) {
+        displayAll = displayAll + "Trip " + parseInt(i+1) + ": <br>" + tripArr[i].name + 
+        "<br>" + tripArr[i].destination + "<br>" + tripArr[i].month  + "<br>" + tripArr[i].transportation  
+        + "<br>" + tripArr[i].numPeople + "<br>" + border + "<br>";
+    }
+
+    document.getElementById("listAll").innerHTML = displayAll;
+});
+
+
+document.getElementById("buttonTripFacts").addEventListener("click", function(){
+    let totalTripNumber = tripArr.length;
+    let totalPeopleNumber = 0;
+    
+    for (let i = 0; i < tripArr.length; i++) {
+       totalPeopleNumber = parseInt(totalPeopleNumber) + parseInt(tripArr[i].numPeople);
+    }
+
+    document.getElementById("tripFacts").innerHTML = "Number of trips: " + totalTripNumber + "<br>Number of people: " + totalPeopleNumber;
     
 });
